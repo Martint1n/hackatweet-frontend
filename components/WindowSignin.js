b57c3991d3c57f4e8b387499d4c082c36f7b29e6
Your name <Your email>
2024-05-03 12:35:30 +0200

    front

--- components/WindowSignin.js
+++ components/WindowSignin.js
@@ -2,6 +2,7 @@ import styles from "../styles/WindowSignin.module.css";
 import {Button, Modal } from 'antd';
 import { useState } from 'react';
 import Image from "next/image"
+import Link from 'next/link';
 
 function WindowSignIn() {
   const [isModalOpen, setIsModalOpen] = useState(false);  // constante qui verifie si le modal est ouvert qui d'origine fermé
@@ -11,6 +12,7 @@ function WindowSignIn() {
     console.log('ok')
     setIsModalOpen(true);
   };
+  const [data, setData] = useState({ result: false });    // constante qui premet de creer un etat d'origine a false sur le data.result
 
   const handleCancel = () => {                            // constante qui contient une fonction qui permet de fermer le modal
     setIsModalOpen(false);
@@ -18,30 +20,38 @@ function WindowSignIn() {
 
   const handleSubmit = () => {                     // permet d'envoyer le contenu des 3 inputs au backend
     console.log(username,password);
-
-    fetch('http://localhost:3000/users/signup', {
+  
+    useEffect(() => {
+    fetch('http://localhost:3000/users/signin', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json' },  // envoie du contenu des inputs au back via un fetch post
       body: JSON.stringify({username,password})
     
     })
       .then(response => response.json())
-      .then(data => {
+      .then(data => { 
+         setData(data)                                  // on renvoie le data.result dans la variable data
         console.log(data);
+  }, []);
+      
     });
   }
-
+            // creation d'un link entourant le button qui permet de nous envoyer sur la page tweet SI le data.result est true sinon nous restons sur le modal
   return (
     <>
     <Button type="primary" onClick={showModal}>          
         SignIn
-      </Button>        // ci dessous, l'eefet du modal avec le style et la variable handleCancel
+      </Button>        // ci dessous, l'effet du modal avec le style et la variable handleCancel
     <Modal title="" footer={null} bodyStyle={{'background-color': 'red', 'padding': '0px', 'margin': '0px'}} open={isModalOpen} onCancel={handleCancel} className={styles.WindowSignin}>
             <Image className={styles.icon} src="/logo.png" alt="logo twitter" height={50} width={50}/>
           <h3 className={styles.h3}>Connect to Hackatweet</h3>
           <input onChange={(e) => setUsername (e.target.value)} value={username} className={styles.inputName} type="text" placeholder="Username" id="signInUsername"></input>
           <input onChange={(e) => setPassword(e.target.value)} value={password} className={styles.inputPassword}type="text" placeholder="Password" id="signInPassword"></input>
-          <button onClick={() => handleSubmit()} className={styles.buttonSignin}>Sign in</button>
+          {data.result ? (
+              <link href="/HomeTwitter"/>
+                ) : (
+                  <button onClick={() => handleSubmit()} className={styles.buttonSignin}>Sign in</button>
+                )}
       </Modal>
     </>
   );
---
+++ pages/HomeTwitter.js
@@ -0,0 +1,8 @@
+import Style from '../styles/HomeTwitter.module.css';
+import HomeTwitter from '../components/HomeTwitter';
+
+function HomeTwitterPage() {
+    return <HomeTwitter />;
+   }
+   
+   export default HomeTwitter;
